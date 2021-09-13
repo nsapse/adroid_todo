af802cc75197b57dee33406cda50c73f2e333549
Noah Sapse <nsapse@gmail.com>
2021-09-13 16:26:58 -0400

    Persistence works, README updated

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -5,13 +5,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+//import android.os.FileUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,11 +43,7 @@ public class MainActivity extends AppCompatActivity {
         rvItems = findViewById(R.id.rvItems);
 
         // Write mock data
-        items = new ArrayList<>();
-        items.add("Buy milk");
-        items.add("Learn Kotlin");
-        items.add("Fix small font");
-        items.add("Finish Book");
+        loadItems();
 
         ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener(){
             @Override
@@ -47,7 +51,8 @@ public class MainActivity extends AppCompatActivity {
                // Delete the item from the model
                 items.remove(Position);
                 itemsAdapter.notifyItemRemoved(Position);
-                Toast.makeText(getApplicationContext(), "item has been removed", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Item Has Been Removed", Toast.LENGTH_SHORT).show();
+                saveItems();
             }
         };
 
@@ -70,9 +75,36 @@ public class MainActivity extends AppCompatActivity {
 
                 // notify the user of the add with a toast
                 Toast.makeText(getApplicationContext(), "item has been added", Toast.LENGTH_SHORT).show();
+                saveItems();
 
             }
         });
 
     }
+
+    // Data Persistence
+
+    // Get Data
+    private File getDataFile(){
+        return new File(getFilesDir(), "data.txt");
+    }
+
+    // Read Data From File
+   private void loadItems(){
+       try {
+           items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+       } catch (IOException e) {
+           Log.e("MainActivity", "Error Reading Items", e);
+           items = new ArrayList<>();
+       }
+   }
+   // Write Data to File
+    private void saveItems(){
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error Writing Items", e);
+        }
+    }
+
  }
